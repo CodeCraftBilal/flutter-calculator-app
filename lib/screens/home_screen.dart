33097da09b579635e18e0f5e8685722b2b976a32
Muhammad Bilal Khan <bilalkhan751150@gmail.com>
2025-06-21 12:03:53 +0500

    button designin complete

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:calculator/constant/colors.dart';
 import 'package:calculator/screens/widgets_data.dart';
 import 'package:flutter/material.dart';
-import '../widgets/button.dart';
 import '../widgets/textfield.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,49 +10,81 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.sizeOf(context).height;
     final padding = EdgeInsets.symmetric(horizontal: 25, vertical: 30);
-    final decoration = BoxDecoration(color: AppColors.primaryColor, borderRadius: BorderRadius.vertical(top: Radius.circular(30)));
+    final decoration = BoxDecoration(
+      color: AppColors.primaryColor,
+      borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+    );
     return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("Calculator"),
+        foregroundColor: Colors.white,
         backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: Text("Calculator"),
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.black,
-        ),
-        body: Column(
-          children: [
-            CustomTextField(),
-            const Spacer(),
-            Container(height: screenHeight * 0.6 ,
+      ),
+      body: Column(
+        children: [
+          CustomTextField(),
+          const Spacer(),
+          Container(
+            height: screenHeight * 0.6,
             width: double.infinity,
             padding: padding,
             decoration: decoration,
-              child: Column(children: [
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: List.generate( 4 , (index) => buttonList[index])
+                  children: List.generate(4, (index) => buttonList[index]),
                 ),
                 Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: List.generate( 4 , (index) => buttonList[index+4])
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: List.generate(4, (index) => buttonList[index + 4]),
                 ),
                 Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: List.generate( 4 , (index) => buttonList[index+8])
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: List.generate(4, (index) => buttonList[index + 8]),
                 ),
                 Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: List.generate( 4 , (index) => buttonList[index + 12])
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: List.generate(
+                              3,
+                              (index) => buttonList[index + 12],
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: List.generate(
+                              3,
+                              (index) => buttonList[index + 15],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20,),
+                    Container(
+                      height: 160,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          color: AppColors.secondaryColor,
+                          borderRadius: BorderRadius.circular(40)
+                      ),
+                      child: Center(child: Text("=", style: TextStyle(fontSize: 32),),),
+                    )
+                  ],
                 ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: List.generate( 4 , (index) => buttonList[index + 12])
-                ),
-              ],),
+              ],
             ),
-          ],
-
-        )
+          ),
+        ],
+      ),
     );
   }
 }
-
